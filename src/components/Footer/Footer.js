7fd7faeb46aa328674a9c375143a9812023ace8d
anurag-s01 <70473436+anurag-s01@users.noreply.github.com>
2021-02-21 00:04:41 +0530

    Add files via upload
    test file for project - TAP portal uploaded

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer px-0 px-lg-3">
+        <Container fluid>
+          
+            
+        </Container>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
