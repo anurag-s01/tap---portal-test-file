fb408e470b2d669bee878d7817e7f0b53bcfe6c5
Anurag Singh <anuragsingh11099@gmail.com>
2021-02-22 22:20:56 +0530

    Home page Modified

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -9,15 +9,22 @@ function Dashboard() {
     <>
       <Container fluid style={{  
             backgroundImage: "url("+Background+")",
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            opacity:"1"
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat"
            }}>
-        <h2 style={{color:"white"}}>Introduction to TAP</h2>
+           <br></br>
+      <div style={{
+        backgroundColor : "rgba(179,185,191,0.3)",
+        borderRadius :"2vh",
+        padding : "20px 40px",
+        WebkitBackdropFilter :"blur(10px)",
+        backdropFilter: "blur(3px)"
+      }}>
+        <h2 style={{color:"white"}}>Welcome to TAP</h2>
         <br></br>
         <br></br>
-        <p style={{fontSize:"22px",color:"white"}}>The Training & Placement Cell, NIT Jamshedpur facilitates the process of placement 
+        <p style={{fontSize:"1.5vw",color:"white"}}>The Training & Placement Cell, NIT Jamshedpur facilitates the process of placement 
         of students passing out from the Institute besides collaborating with leading organizations and institutes in setting 
         up of internship and training program of students. The office liaises with various industrial establishments, corporate
         houses etc which conduct campus interviews and select graduate and post-graduate students from all disciplines. 
@@ -26,7 +33,7 @@ function Dashboard() {
         visit the campus for holding campus interviews.The industries which approach the institute come under the purview of:
         </p>
          <br></br>
-         <ul style={{fontSize:"22px",color:"white"}}>
+         <ul style={{fontSize:"1.5vw",color:"white"}}>
            <li>Core Engineering industries</li>
            <li>IT & IT enabled services</li>
            <li>Manufacturing Industries</li>
@@ -35,6 +42,17 @@ function Dashboard() {
            <li>Management Organisations</li>
            <li>R & D laboratories</li>
          </ul>
+         <br></br>
+         <p style={{fontSize:"1.5vw",color:"white"}}>The placement season runs through the course of the year commencing the last week of July through to June. Pre-Placement Talks are also conducted in this regard as per mutual convenience.
+         Job offers, dates of interviews, selection of candidates etc. are announced through the Training & Placement Cell.
+         The Placement Office is assisted by a committee comprising representatives of students from the under-graduate and post-graduate engineering streams.
+         The committee evolves a broad policy framework every year besides a set of rules which are inviolable. Students members are closely co-opted in implementing these policy decisions.</p>
+         <br></br>
+         <p style={{fontSize:"1.3vw",color:"white"}}> Other informations can be accessed from 
+          <a href="http://www.nitjsr.ac.in/tap/portfolio/"> here</a>
+          </p>
+          </div>
+          <br/>
       </Container>
     </>
   );
