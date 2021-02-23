3b3c7f824e2c9d0765b5388d9fa8b5f7b79f339f
Anurag Singh <anuragsingh11099@gmail.com>
2021-02-23 06:19:41 +0530

    Reduced code size

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -97,15 +97,7 @@ function Header() {
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-            <Nav.Item>
-              <Nav.Link
-                className="m-0"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="no-icon">Account</span>
-              </Nav.Link>
-            </Nav.Item>
+           
           </Nav>
           <Nav className="ml-auto" navbar>
           <Nav.Item>
